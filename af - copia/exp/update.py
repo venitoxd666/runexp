119c6ebcaaab8b3d271e9ea98d74740b6727d094
venitoxd666 <95440696+venitoxd666@users.noreply.github.com>
2023-02-26 13:58:00 +0100

    Add files via upload

--- af - copia/exp/update.py
+++ af - copia/exp/update.py
@@ -19,12 +19,14 @@ def _update(expname):
     )
     if(req.status_code != 200):
         rich.print("[red]ERROR[/red]: Invalid request status: %s" % req.status_code)
+    req_text = req.text.replace("\r\n", "\n")
     try:
-        shall_replace = 1
-        with open(os.path.join(af_path(), expname + '.py'), 'r') as f:
-            current_hash = sha256(f).hexdigest()
-            if(sha256(req.text).hexdigest() == current_hash):
-                shall_replace = 0
+        shall_replace = 0
+        
+        with open(os.path.join(af_path(), expname + '.py'), 'r', encoding='utf-8') as f:
+            
+            f.seek(0)
+            shall_replace = f.read() != req_text
 
     except Exception as e:
         shall_replace = 2
@@ -33,24 +35,23 @@ def _update(expname):
         rich.print("Already in last version")
         return
     
-    if(shall_replace == 2):
+    if(shall_replace):
         rich.print("Installing new EXP...")
     
     sys.stdout.write("[%s]" % (" " * 100))
     sys.stdout.flush()
     sys.stdout.write("\b" * (100+1)) # return to start of line, after '['
-    step = len(req.text)//100 + 1
-    with open(os.path.join(af_path(), expname + '.py'), 'w') as f:
-        for index, char in enumerate(req.text):
+    step = len(req_text)//100 + 1
+    with open(os.path.join(af_path(), expname + '.py'), 'w', encoding='utf-8') as f:
+        for index, char in enumerate(req_text):
             if(index % step == 0):
                 sys.stdout.write("-")
                 sys.stdout.flush()
             try:
-                if(char == '\r'):
-                    continue
                 f.write(char)
             except KeyboardInterrupt:
-                pass
+                # cant interrupt the installation process as that would mean the file hasnt copied correctly.
+                f.write(char)
     sys.stdout.write("\n")
 
 def main():
