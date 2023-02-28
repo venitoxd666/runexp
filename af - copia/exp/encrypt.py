52f9b34fb36e1adfd2ccabb3e17a02f8b2e4d7c1
venitoxd666 <95440696+venitoxd666@users.noreply.github.com>
2023-02-28 23:04:46 +0100

    Add files via upload

--- af - copia/exp/encrypt.py
+++ af - copia/exp/encrypt.py
@@ -10,7 +10,7 @@ import argparse
 import rich
 import random
 from maskpass import askpass
-import heapq
+from collections import OrderedDict
 
 ap = argparse.ArgumentParser(
     prog = "encrypt"
@@ -23,14 +23,18 @@ ap.add_argument('-m', '--method', dest = 'method', help = "method to encrypt.")
 ap.add_argument('-u', "--undo", dest = "dercrypt", action = "store_true", help = "especify wether is encrpyt or decrypt")
 ap.add_argument('-p', '--password', dest = "password", help = "password to decrypt or encrypt. \"GEN()\" to generate and print it.")
 
+ap.add_argument('-t', '--repeat', dest='times', help="Number of times to repeat the encryption", type=int, default = 1)
+
+
 ap.add_argument('--password_mask', dest='mask', help="Change the password input mask", default = "*")
 
-
 HELP = """
-Version: 1.0.0
+Version: 1.1.0
 
 Changelog:
     1.0.0: -initial behaviour implemented
+        1.1.0: Added encryption methods: ntm, ntg, huffman
+               Better representation and user interface
 
 encrpyt [purple]encrypts[/purple] a file many ways.
 (to encrypt a directory use "-d" (check))
@@ -41,10 +45,13 @@ Valid values for "-m" | "--method" are:
         Not encryption, simple, not really encrypting anything, just changing the format and the most basic encryption method
     -m [red]ntg[/red]
         Symmetric encryption, integer password between 0 and 255.
-        
-ntm: size complexity: S(n) -> O(n)
-ntg: size complexity: S(n) -> O(2*7 + 2*n)
-
+    -m [red]huffman[/red]
+        multi-state machine encryption, the message depends on the password, as bigger
+        the message, as harder to crack the password. And so as bigger the password.
+    
+ntm: size complexity:     S(n) -> O(n)
+ntg: size complexity:     S(n) -> O(2*7 + 2*n)
+huffman: size complexity: S(n) -> ~
 """
 
 def print_help():
@@ -53,25 +60,237 @@ def print_help():
 
 ap.print_help = print_help
 
+encrypter = None
+
+def get_encrypter():
+    return encrypter
+
+def set_encrypter(enc):
+    global encrypter
+    return encrypter
+
+
 def _INT_NOT(i, numbits=8):
     return (1 << numbits) - 1 - i
 
 class Diagram(object):
-    def __init__(self, value,):
-        self.value = value
-        self.children = []
-        self.parent = None
+    def __init__(self, token, parent=None):
+        self.token = token
+        self.children = {}
+        self.parent = parent
+    
+    def add_state(self, state, token):
+        if(token == self.token):
+            self.children[state] = self
+            return self
+        if(state in self.children):
+            return self.children[state]
+        
+        self.children[state] = Diagram(token, parent=self)
+        return self.children[state]
+
+    def __repr__(self):
+        return f"Diagram<{self.token} states:{list(self.children.keys())}>"
 
 class Huffman(object):
-    def __init__(self, buffer):
+    def __init__(self, encrypter:"Encrypter", buffer):
         self.buffer = buffer
+        self.encrypter = encrypter
+        self.psswd = self.process_password(self.encrypter.need_password())
+        if(len(self.psswd) < 10):
+            self.encrypter.error("Password too short, massive data loss if password too short. Recomended to write something big", 1)
+        self._encrypt = True
     
-    def compute(self):
-        pass
-    
-    def decrypt(self):
-        pass
+    def process_password(self, password:str):
+        vector = list(password)
+        res = ""
+        last = None        
+        last_count = 0
+        for obj in vector:
+            if(obj == last):
+                last_count += 1
+                continue
+            else:
+                if(last_count >= 1):
+                    res += chr(last_count)
+                res += last if last else ""
+                last = obj
+                last_count = 0
+        if(last_count > 1):
+            res += chr(last_count)
+        res += last if last else ""
+        last = obj
+        _res =  res.encode()
+        _res += _res[:4] # a little of extra space in the password wich makes it harder to crack
+                         # the encoding
+        addup = sum(_res)
+        _res += int.to_bytes(addup, 
+                             (addup.bit_length() // 8 + 1),
+                             'big'
+                            )
+        _res += int.to_bytes(addup, 
+                             (addup.bit_length() // 8 + 1),
+                             'little'
+                            )
 
+        _res += b'\xff'
+        return _res
+        
+    def get_diagram(self):
+        # works, DO NOT DARE TO TOUCH THIS SHIT
+        # or ELSE IT CAN DESTROY EVERYTHING THAT HAS TO DO WITH HUFFMAN
+        char = self.buffer.read(1)
+        root = last = Diagram(char)
+        count = -1
+        mapping = {char:root}
+        while(char):
+            # char advance
+            char = self.buffer.read(1)
+            if not(char):
+                continue
+
+            # state / password advance
+            count += 1
+            if(count == len(self.psswd)):
+                count = 0   
+
+            # mapping
+            if(char in mapping):
+                # already mapped, just add a state to it
+                last.children[self.psswd[count]] = mapping[char]
+                last = mapping[char]
+            else:
+                last = last.add_state(self.psswd[count], char)
+                mapping[char] = last
+        return root
+
+    def flatten(self, root, saved:list):
+        if(root in saved):
+            return
+        res = OrderedDict()
+        res[root.token] = root
+        
+        saved.append(root)
+        
+        for obj in root.children.values():
+            _upd = self.flatten(obj, saved)
+            if not(_upd):
+                continue 
+            res.update(_upd)
+        
+        return res
+
+    def a_getvalue(self):
+        res = io.BytesIO()
+        current = self.get_diagram()
+        if not(current):
+            return res.getvalue()
+        
+        saved = list()
+        flat = self.flatten(current, saved)
+
+        for obj in flat:
+            bj,refs = flat[obj],flat[obj].children
+            res.write(
+                int.to_bytes(
+                    len(refs),
+                    1,
+                    'big'
+                )
+            )
+            for state, obj in refs.items():
+                res.write(
+                    int.to_bytes(
+                        state,
+                        1,
+                        'big'
+                    )
+                )
+                res.write(
+                    int.to_bytes(
+                        saved.index(obj),
+                        1,
+                        'big'
+                    )
+                )
+            res.write(bj.token)
+
+        res.seek(0)
+        return res.getvalue()
+
+
+    def b_getvalue(self):
+        def _ensure_read(index):
+            char = self.buffer.read(index)
+            if not(char):
+                self.encrypter.error(
+                    "File format corrupt for huffman encryption", 1)
+            return char
+
+        def read_chunk():
+            number_of_refs = _ensure_read(1)[0]
+            refs = {}
+            for i in range(number_of_refs):
+                case = _ensure_read(1)
+                index = _ensure_read(1)[0]
+                refs[case] = index
+            return {
+                "refs":refs,
+                "token":_ensure_read(1)
+            }
+        
+        def _lazy_char():
+            backup = self.buffer.tell()
+            res = self.buffer.read(1)
+            self.buffer.seek(backup)
+            return res
+
+        chunks = []
+        while(_lazy_char()):
+            chunks.append(read_chunk())
+        
+        current_chunk_index = 0
+        count = -1
+        res = io.BytesIO()
+
+        while(True):
+            count += 1
+            if(count == len(self.psswd)):
+                count = 0
+            
+            current = chunks[current_chunk_index]
+
+            case = int.to_bytes(self.psswd[count],1,'big')
+
+            refs = current['refs']
+            
+            res.write(current['token'])
+            
+            if not len(refs):
+                break
+
+            if not(case in refs):
+                break
+            current_chunk_index = refs[case]
+            
+        return res.getvalue()
+
+    def c_getvalue(self):
+        if(self._encrypt):
+            return self.a_getvalue()
+        return self.b_getvalue()
+
+    def compute(self):
+        self._encrypt=True
+        v = self.c_getvalue()
+        self.getvalue = lambda *a,**k:v
+        return self
+
+    def decrypt(self):
+        self._encrypt=False
+        v = self.c_getvalue()
+        self.getvalue = lambda *a,**k:v
+        return self
 
 def req_ps(method):
     method.requires_password = True
@@ -81,6 +300,7 @@ class Encrypter(object):
     def __init__(self, opts):
         self.opts = opts
         self._check_args()
+        set_encrypter(self)
         
     def _check_args(self):
         if not(self.opts.file) and not(self.opts.directory):
@@ -140,8 +360,9 @@ class Encrypter(object):
 
         return res
     
+    @req_ps
     def huffman(self, read_buffer):
-        huffman = Huffman(read_buffer)
+        huffman = Huffman(self,read_buffer)
         return huffman.compute()
 
     # decryption methods
@@ -150,7 +371,10 @@ class Encrypter(object):
 
     def need_password(self, prompt = "Enter password:"):
         if not(self.opts.password):
-            self.opts.password = askpass(prompt,mask=self.opts.mask)           
+            try:
+                self.opts.password = askpass(prompt,mask=self.opts.mask)
+            except KeyboardInterrupt:
+                self.error("KeyboardInterrupt", -255)
         return self.opts.password
 
     @req_ps
@@ -184,26 +408,28 @@ class Encrypter(object):
 
         return res
 
+    @req_ps
     def huffman_decrypt(self, read_buffer):
-        return Huffman(read_buffer).decrypt()
+        return Huffman(self,read_buffer).decrypt()
 
     # keep working functionality
-    def encrypt(self, method, file, output,stop):
-        file = open(file, 'rb')
-        try:
+    def encrypt(self, method, fl, output,stop):
+        for i in range(self.opts.times):
+            file = open(fl, 'rb')
+            try:
+                if(stop()):
+                    return
+                output_buffer = method(file)
+            except Exception as e:
+                self.error(f"Invalid method: {method}",1)
+            file.close()
             if(stop()):
                 return
-            output_buffer = method(file)
-        except Exception as e:
-            self.error(f"Invalid method: {method}",1)
-        file.close()
-        if(stop()):
-            return
 
-        with open(output, 'wb') as file:
-            if(stop()):
-                return
-            file.write(output_buffer.getvalue())
+            with open(output, 'wb') as file:
+                if(stop()):
+                    return
+                file.write(output_buffer.getvalue())
         
     def error(self, msg, errno):
        rich.print(f"[red]ERROR({errno})[/red]: {msg}") 
@@ -241,7 +467,7 @@ class Encrypter(object):
             while(thread.is_alive()):
                 try:
                     char = chars[state % len(chars)]
-                    text = f'[{char}] - {file} >> {output} '
+                    text = f'[{char}] - {file} >> {output}\n'
                     sys.stdout.write("\033[F" + text)
                     sys.stdout.flush()
                     time.sleep(0.2)
